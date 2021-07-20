d1bb69d76b3aca94084fdbbb02c5ec906a23c207
lotfi-firdaouss <lotfi.firdaouss@gmail.com>
2021-07-20 10:53:50 +0100

    adding rest_framework, psycopg2 adapter and configuring the CORS

--- platform_backend/settings.py
+++ platform_backend/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'main_app.apps.MainAppConfig',
+    # CORS
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -47,7 +51,15 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # CORS
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:8081',
+)
 
 ROOT_URLCONF = 'platform_backend.urls'
 
@@ -75,8 +87,12 @@ WSGI_APPLICATION = 'platform_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'platform_db',
+        'USER': 'postgres',
+        'PASSWORD': '1.postgreAdmin.1',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
