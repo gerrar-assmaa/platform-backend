e735c6515639c7ac80bae53ddfc3c7cf8e588ffb
gerrar-assmaa <gerrar.assmaa@ensam-casa.ma>
2021-08-12 16:32:34 +0100

    registration with google

--- signIn_Up/serializer.py
+++ signIn_Up/serializer.py
@@ -1,7 +1,7 @@
 from rest_framework import  serializers
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate
-from django.contrib.auth.hashers import make_password
+from rest_framework.validators import UniqueValidator
+
 
 # Register serializer
 class RegisterSerializer(serializers.ModelSerializer):
@@ -10,21 +10,22 @@ class RegisterSerializer(serializers.ModelSerializer):
         fields = ('id','username','password','first_name', 'last_name','email','groups')
         extra_kwargs = {
             'password':{'write_only': True},
+            'email':{ 'validators': [
+                UniqueValidator(queryset= User.objects.all())
+            ] 
+            }
         }
     def create(self, validated_data):
-        """user = User.objects.create_user(validated_data['username'],password = validated_data['password']  ,first_name=validated_data['first_name'],  last_name=validated_data['last_name'],
-        email= validated_data['email'])"""
 
         user = User.objects.create_user(
             username=validated_data['username'],
             email= validated_data['email'],
+            first_name= validated_data['first_name'],
+            last_name= validated_data['last_name'],
             password = validated_data['password']
         )
 
-        """ 
-        first_name= validated_data['first_name'],
-            last_name= validated_data['last_name'],
-        """
+        
         group = validated_data['groups'][0]
         user.groups.add(group)
         return user
