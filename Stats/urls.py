405ff969d228bec4f1d0da2768918a2944d9efa1
lotfi-firdaouss <lotfi.firdaouss@gmail.com>
2021-09-08 17:17:07 +0100

    deleting unecessary files

--- Stats/urls.py
+++
@@ -1,7 +0,0 @@
-from main_app import views
-from django.urls import path
-from . import views
-
-urlpatterns = [
-  path('reportsFile',views.download_excel_data)
-]
--- platform_backend/urls.py
+++ platform_backend/urls.py
@@ -1,14 +1,12 @@
 from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
-from django.conf.urls.static import static
 import debug_toolbar
 
 urlpatterns = [
   #path('admin/', admin.site.urls),
   path('api/',include('main_app.urls')),
   path('sign/',include('signIn_Up.urls')),
-  path('downFiles/',include('Stats.urls')),
   path('__debug__/', include(debug_toolbar.urls)),
 ]
 
